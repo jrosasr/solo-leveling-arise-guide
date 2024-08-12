4f82a97fdc3a5324bfdb0c7f9c487de5cab7df9d
Juan Rosas <juan.rosasr01@gmail.com>
2024-08-12 16:22:20 -0400

    slider base

--- app/page.js
+++ app/page.js
@@ -1,113 +1,128 @@
-import Image from "next/image";
+'use client'
+import { useEffect } from 'react'
 
+const CHARACTERS = [
+  {
+    name: 'Sung Jinwoo',
+    slug: "sung-jinwoo.png",
+  },
+  {
+    name: 'Alicia Blanche',
+    slug: "alicia-blanche.png",
+  },
+  {
+    name: 'Cha Hae In',
+    slug: "cha-hae-in.png",
+  },
+  {
+    name: 'Lee Joohee',
+    slug: "lee-joohee.png",
+  },
+  {
+    name: 'Lee Joohee',
+    slug: "lee-joohee.png",
+  },
+  {
+    name: 'Lee Joohee',
+    slug: "lee-joohee.png",
+  },
+  {
+    name: 'Lee Joohee',
+    slug: "lee-joohee.png",
+  },
+  {
+    name: 'Lee Joohee',
+    slug: "lee-joohee.png",
+  },
+  {
+    name: 'Lee Joohee',
+    slug: "lee-joohee.png",
+  },
+  {
+    name: 'Song Chiyul',
+    slug: "song-chiyul.png",
+  }
+]
 export default function Home() {
+  useEffect(() => {
+    const thumbs = document.querySelectorAll('.thumb li')
+    const infoSlider = document.querySelectorAll('.info-slider')
+    const imgSlider = document.querySelectorAll('.img-slider')
+    const items = document.querySelectorAll('.item')
+
+    let indet = 0
+
+    thumbs.forEach((thumb, ind) => {
+      thumb.addEventListener('click', () => {
+        document.querySelector('.thumb .selected')?.classList?.remove('selected')
+        thumb.classList.add('selected')
+
+        const index = ind
+
+        infoSlider.forEach(slide => {
+          slide.style.transform = `translateY(${index * -100}%)`
+        })
+
+        imgSlider.forEach(slide => {
+          slide.style.transform = `translateX(${index * -100}%)`
+        })
+
+        document.querySelector('.item.active').classList.remove('active')
+        items[index].classList.add('active')
+      })
+    })
+  
+    return () => {
+      document.querySelector('.item.active')?.classList?.remove('active')
+      items[0].classList.add('active')
+    }
+  }, [])
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main classList="max-w-7xl mx-auto">
+
+      </main>
+      <div className="carousel">
+        {CHARACTERS.map((character, i) => (
+          <div className="item" key={i}>
+          <div className="bg"></div>
+          <div className="info-box">
+            <div className="info-slider">
+              <div className="info-item" style={{ '--i': i }}>
+                <h2>{character.name}</h2>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and 
+                  typesetting industry. Lorem Ipsum has been the industrys 
+                  standard dummy text ever since the 1500s, when an unknown 
+                  printer took a galley of type and scrambled it to make a 
+                  type specimen book. It has survived not only five centuries
+                </p>
+                <a className="btn" href="#">View More</a>
+              </div>
+            </div>
+          </div>
+          <div className="img-box">
+            <div className="img-slider">
+              <div className="img-item" style={{ '--i': i }}>
+                <img src={`/characters/${character.slug}`} />
+              </div>
+            </div>
+          </div>
         </div>
+        ))}
+
+        <ul className="thumb">
+          {CHARACTERS.map((character, i) => (
+            <li key={i}>
+              <img src={`/miniatures/${character.slug}`} />
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //   asdasd
+    // </main>
   );
 }
