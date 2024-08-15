9eb0b014dc312d31d5137ffad960852d3789984c
Juan Rosas <juan.rosasr01@gmail.com>
2024-08-15 13:27:35 -0400

    bug responsive

--- components/CharacterSlider/CharacterDescription.jsx
+++ components/CharacterSlider/CharacterDescription.jsx
@@ -1,7 +1,7 @@
 export default function CharacterDescription({ title, description, index }) {
   return (
-    <div className="info-box w-12">
-      <div className="info-slider w-3/4 md:w-full h-auto">
+    <div className="info-box">
+      <div className="info-slider w-3/4 md:w-6/12 h-auto">
         <div className="info-item" style={{ "--i": index }}>
           <h2>{title}</h2>
           <p className="">
--- components/CharacterSlider/CharacterMainImage.jsx
+++ components/CharacterSlider/CharacterMainImage.jsx
@@ -1,7 +1,7 @@
 export default function CharacterMain({ slugImage, index }) {
   return (
     // 
-    <div className="right-[-5%] md:right-[0%] w-[800px] h-[700px] img-box">
+    <div className="right-[-5%] md:right-[0%] mr-[5%] h-[700px] img-box">
       <div className="img-slider">
         <div className="img-item" style={{ "--i": index }}>
           <img width={500} height={450} src={`/characters/${slugImage}`} className="ml-[50%] md:ml-[30%]" />
